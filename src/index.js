5c39ec6c04b28fcbf051b281681b758e5539002e
darinatsybenko <110915599+darinatsybenko@users.noreply.github.com>
2023-01-20 18:39:36 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+function fetchCountries(name) {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    ).then(responce => {
+      if (!responce.ok) {
+        throw new Error(
+          `Something goes wrong. Responce status: ${responce.status}!`
+        );
+      }
+      return responce.json();
+    });
+  }
+  
+  export { fetchCountries };
