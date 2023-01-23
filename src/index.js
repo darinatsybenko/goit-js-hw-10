a627d9a1fdf067004bfac572fb45697f97a1638f
darinatsybenko <110915599+darinatsybenko@users.noreply.github.com>
2023-01-23 14:33:25 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,94 @@
 import './css/styles.css';
 
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
-function fetchCountries(name) {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    ).then(responce => {
-      if (!responce.ok) {
-        throw new Error(
-          `Something goes wrong. Responce status: ${responce.status}!`
-        );
-      }
-      return responce.json();
-    });
+const warningMessage =
+  'Too many matches found. Please enter a more specific name.';
+const failureMessage = 'Oops, there is no country with that name.';
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(event) {
+  deleteMarkup();
+
+  if (!event.target.value.trim()) {
+    return;
   }
-  
-  export { fetchCountries };
+
+
+
+  fetchCountries(event.target.value.trim())
+    .then(result => {
+      if (result.length > 10) {
+        Notify.warning(warningMessage);
+        return;
+      }
+
+      if (result.length > 1) {
+        createMarkupForFew(result);
+        return;
+      }
+      createMarkupForOne(result);
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.message.includes('404')) {
+        Notify.failure(failureMessage);
+      }
+    });
+}
+
+function createMarkupForFew(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+        <div class="country-item"><img class="country-flag" src="${country.flags.svg}" alt="${country.name.official} flag" height="50">
+        <p class="country-name">${country.name.official}</p>
+        </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+}
+
+function createMarkupForOne(countries) {
+  const markup = countries
+    .map(country => {
+      const languages = Object.values(country.languages).join(', ');
+      return ` <div class="country-info__wrapper"><img src="${country.flags.svg}" alt="${country.name.official} flag" height="50">
+      <h1>${country.name.official}</h1></div>
+      <p><span class ="counry-info--bold">Capital:</span> ${country.capital}</p>
+      <p><span class ="counry-info--bold">Population:</span> ${country.population}</p>
+      <p><span class ="counry-info--bold">Languages:</span> ${languages}</p>`;
+    })
+    .join('');
+  refs.countryInfo.innerHTML = markup;
+}
+
+function deleteMarkup() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(responce => {
+    if (!responce.ok) {
+      throw new Error(
+        `Something goes wrong. Responce status: ${responce.status}!`
+      );
+    }
+    return responce.json();
+  });
+}
+
+export { fetchCountries };
